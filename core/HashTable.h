fbcbe4471c6985f26a261a99e0f212522117470a
mrodenkirch <mrodenkirch@bb85bfea-1d01-4b18-b6d8-510d50b5ba24>
2022-11-22 18:14:25 +0000

    Performance updates
    git-svn-id: https://svn.code.sf.net/p/mtsieve/svn@210 bb85bfea-1d01-4b18-b6d8-510d50b5ba24

--- core/HashTable.h
+++ core/HashTable.h
@@ -13,63 +13,75 @@
 #define HASH_MASK1         (1<<15)
 #define HASH_MASK2         (HASH_MASK1-1)
 #define HASH_MAX_ELTS      HASH_MASK2-1
-#define HASH_MINIMUM_SHIFT 10
+#define HASH_MINIMUM_SHIFT 11
 
 class HashTable
 {
 public:
    HashTable(uint32_t elements);
-   HashTable(uint32_t babySteps, uint32_t bestQ, uint32_t powerResidueLcm);
    
    ~HashTable(void);
 
    void  Cleanup(void);
    
+   uint64_t GetInserts(void) { return il_Inserts; };
+   uint64_t GetConflicts(void) { return il_Conflicts; };
+   
    inline void Clear(void)
    {
-      memset(htable, 0, hsize*sizeof(uint16_t));
+      // hsize is always a power of 2
+      for (uint32_t i=0; i < hsize; i+=2)
+      {
+         htable[i] = empty_slot;
+         htable[i+1] = empty_slot;
+      }
+      
+      // will never match a Lookup value
+      BJ64[empty_slot] = UINT64_MAX;
    }
    
-   inline uint64_t get(uint32_t x) {return BJ64[x]; };
+   inline uint64_t get(uint32_t x) { return BJ64[x]; };
    
-   inline void Insert(uint64_t bj, uint32_t j)
+   inline void  Insert(uint64_t bj, uint32_t j)
    {
       uint32_t slot;
+      il_Inserts++;
 
       BJ64[j] = bj;
       slot = bj & hsize_minus1;
       if (htable[slot] == empty_slot)
-         htable[slot] = j;
-      else
       {
-         olist[j] = (htable[slot] ^ HASH_MASK1);
-         htable[slot] = (j | HASH_MASK1);
+         htable[slot] = j;
+         return;
       }
+
+      olist[j] = htable[slot];
+      htable[slot] = (j | HASH_MASK1);
+      il_Conflicts++;
+
    };
    
    inline uint32_t Lookup(uint64_t bj)
    {
       uint32_t slot;
       uint16_t elt;
+      uint16_t elt_low;
 
       slot = bj & hsize_minus1;
       elt = htable[slot];
+      elt_low = elt & HASH_MASK2;
 
-      if (BJ64[elt & HASH_MASK2] == bj)
-         return elt & HASH_MASK2;
+      // Could check elt == empty_slot to avoid checking BJ64
+      if (BJ64[elt_low] == bj)
+         return elt_low;
       
-      if ((elt & HASH_MASK1) == 0)
-         return HASH_NOT_FOUND;
-
-      elt &= HASH_MASK2;
-      do
-      {
-         if (BJ64[olist[elt] & HASH_MASK2] == bj)
-            return olist[elt] & HASH_MASK2;
-         
-         elt = olist[elt];
-      } while ((elt & HASH_MASK1) == 0);
-
+      while (elt != elt_low) {
+         elt = olist[elt & HASH_MASK2];
+         elt_low = elt & HASH_MASK2;
+         if (BJ64[elt_low] == bj)
+            return elt_low;
+      }
+      
       return HASH_NOT_FOUND;
    };
 
@@ -80,6 +92,9 @@ private:
    uint32_t  hsize;
    uint32_t  hsize_minus1;
    uint64_t *BJ64;
+   
+   uint64_t  il_Inserts;
+   uint64_t  il_Conflicts;
 };
 
 #endif
