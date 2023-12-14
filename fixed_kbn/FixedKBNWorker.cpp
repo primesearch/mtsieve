15fb4e054226c12fe9189ecfb900a2d156a5fce5
mrodenkirch <mrodenkirch@bb85bfea-1d01-4b18-b6d8-510d50b5ba24>
2023-12-14 19:22:01 +0000

    Fix factor validation
    git-svn-id: https://svn.code.sf.net/p/mtsieve/svn@346 bb85bfea-1d01-4b18-b6d8-510d50b5ba24

--- fixed_kbn/FixedKBNWorker.cpp
+++ fixed_kbn/FixedKBNWorker.cpp
@@ -10,18 +10,18 @@
 #include <stdint.h>
 
 #include "FixedKBNWorker.h"
-#include "../x86_asm/fpu-asm-x86.h"
+#include "../core/MpArithVector.h"
 
 FixedKBNWorker::FixedKBNWorker(uint32_t myId, App *theApp) : Worker(myId, theApp)
 {
    ip_FixedKBNApp = (FixedKBNApp *) theApp;
-   
+
    il_K = ip_FixedKBNApp->GetK();
    ii_Base = ip_FixedKBNApp->GetBase();
    ii_N = ip_FixedKBNApp->GetN();
    il_MinC = ip_FixedKBNApp->GetMinC();
    il_MaxC = ip_FixedKBNApp->GetMaxC();
-   
+
    // The thread can't start until initialization is done
    ib_Initialized = true;
 }
@@ -33,9 +33,8 @@ void  FixedKBNWorker::CleanUp(void)
 void  FixedKBNWorker::TestMegaPrimeChunk(void)
 {
    uint64_t ps[4];
-   uint64_t bs[4];
-   uint64_t maxPrime = ip_App->GetMaxPrime();   
-      
+   uint64_t maxPrime = ip_App->GetMaxPrime();
+
    for (uint32_t pIdx=0; pIdx<ii_PrimesInList; pIdx+=4)
    {
       ps[0] = il_PrimeList[pIdx+0];
@@ -43,17 +42,19 @@ void  FixedKBNWorker::TestMegaPrimeChunk(void)
       ps[2] = il_PrimeList[pIdx+2];
       ps[3] = il_PrimeList[pIdx+3];
 
-      bs[0] = bs[1] = bs[2] = bs[3] = ii_Base;
-      
-      fpu_powmod_4b_1n_4p(bs, ii_N, ps);
-   
-      RemoveTerms(ps[0], bs[0]);
-      RemoveTerms(ps[1], bs[1]);
-      RemoveTerms(ps[2], bs[2]);
-      RemoveTerms(ps[3], bs[3]);
+      MpArithVec mp(ps);
+
+      MpResVec res = mp.pow(mp.nToRes(ii_Base), ii_N);
+      res = mp.mul(res, mp.nToRes(il_K));
+      MpResVec kbExpN = mp.resToN(res);
+
+      RemoveTerms(ps[0], kbExpN[0]);
+      RemoveTerms(ps[1], kbExpN[1]);
+      RemoveTerms(ps[2], kbExpN[2]);
+      RemoveTerms(ps[3], kbExpN[3]);
 
       SetLargestPrimeTested(ps[3], 4);
-      
+
       if (ps[3] > maxPrime)
          break;
 
@@ -70,17 +71,10 @@ void  FixedKBNWorker::TestMiniPrimeChunk(uint64_t *miniPrimeChunk)
    FatalError("FixedKBNWorker::TestMiniPrimeChunk not implemented");
 }
 
-void    FixedKBNWorker::RemoveTerms(uint64_t prime, uint64_t bExpN)
+void    FixedKBNWorker::RemoveTerms(uint64_t prime, uint64_t kbExpN)
 {
    int64_t  c;
-   int64_t  kbExpN;
-   
-   fpu_push_1divp(prime);
-   
-   kbExpN = fpu_mulmod(il_K, bExpN, prime);
-   
-   fpu_pop();
-      
+
    // k*b^n (mod p) = kbExpN
    // k*b^n - kbExpN (mod p) = 0
    // k*b^n + (p - kbExpN) (mod p) = 0
@@ -92,22 +86,22 @@ void    FixedKBNWorker::RemoveTerms(uint64_t prime, uint64_t bExpN)
 
    int countVerified = 0;
 
-   do 
+   do
    {
       // k*b^n + c (mod p) = 0
       ip_FixedKBNApp->ReportFactor(prime, c, (++countVerified < 5));
 
       c += (int64_t) prime;
    } while (c <= il_MaxC);
-   
+
    c = (int64_t) (prime - kbExpN);
 
    countVerified = 0;
-   do 
+   do
    {
       // k*b^n + c (mod p) = 0
       ip_FixedKBNApp->ReportFactor(prime, c, (++countVerified < 5));
-      
+
       c -= (int64_t) prime;
    } while (c >= il_MinC);
 }
